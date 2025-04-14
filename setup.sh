a0dcdabda53bc7ddee9724ffff6982087fb7cdf9
levi <levi@transtrum.net>
2025-04-14 13:02:07 -0700

    add setup.sh for convenience

---
+++ setup.sh
@@ -0,0 +1,18 @@
+# Note: Need to have current date and time set with
+# `sudo date -s YYYY-MM-DD HH:MM:SS`
+# or else you will run into issue with certificates
+
+# Clone the code
+git clone https://github.com/LeviTranstrum/gesture
+
+# Install dependencies
+# Pillow dependencies
+sudo apt update
+sudo apt install -y libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjpeg-dev libtiff-dev libwebp-dev tcl-dev tk-dev python3-dev python3-pip
+
+# LiteRT (formerly TFLite ) wheel
+curl -k -OL https://github.com/LeviTranstrum/yokogawa-ert3/raw/master/tflite_runtime-2.6.0-cp39-cp39-linux_armv7l.whl
+pip install tflite_runtime-2.6.0-cp39-cp39-linux_armv7l.whl
+
+# Other pip packages
+pip install -r gesture/requirements.txt
