65b6d64ec307d8e7ea6942ae952e3c9314f88f45
levi <levi@transtrum.net>
2025-04-15 00:01:34 +0000

    make setup.sh executable

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sudo ntpdate pool.ntp.org
 # Install dependencies
 # Pillow dependencies
 sudo apt update
-sudo apt install -y libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjpeg-dev libtiff-dev libwebp-dev tcl-dev tk-dev python3-dev python3-pip
+sudo apt install -y libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libtiff-dev libwebp-dev tcl-dev tk-dev python3-dev python3-pip
 
 # LiteRT (formerly TFLite ) wheel
 curl -k -OL https://github.com/LeviTranstrum/yokogawa-ert3/raw/master/tflite_runtime-2.6.0-cp39-cp39-linux_armv7l.whl
